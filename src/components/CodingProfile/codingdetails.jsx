a0d8c51a8ce54e05dda62c05d4dc2a13afe95c65
Tarun <tarunroy500@gmail.com>
2023-09-21 13:59:09 +0530

    icons-react

--- src/components/CodingProfile/codingdetails.jsx
+++ src/components/CodingProfile/codingdetails.jsx
@@ -1,18 +1,18 @@
 const CodingProfileDetails = [
     {
         name: "LEETCODE",
-        rating: "1641",
-        problem: "200+",
-        contest: "",
-        rank: "325,302",
+        rating: "1474",
+        problem: "350+",
+        contest: "2",
+        rank: "226,599",
         link: "https://leetcode.com/tarunroy500/"
     },
     {
         name: "GFG",
-        rating: "90",
-        problem: "30+",
+        rating: "196",
+        problem: "60+",
         contest: "6+",
-        rank: "77",
+        rank: "71",
         link: "https://auth.geeksforgeeks.org/user/tarunroy500/practice"
     },
     {
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,6 +1,8 @@
 import "../skills/skills.css";
 import Data from "../../Data2";
 import {FaHtml5} from 'react-icons/fa'
+import {FaNodeJs,FaBootstrap,FaJava,FaBrain,FaCss3Alt,FaReact} from 'react-icons/fa'
+import {TbBrandCpp} from 'react-icons/tb'
 import {SiJavascript} from 'react-icons/si'
 import Card from "../../Card2";
 const Skills = () => {
@@ -23,7 +25,7 @@ const Skills = () => {
                     <div class="badge yellow">
                         <div class="circle">
                             {" "}
-                            <i class="fa fa-brands fa-react"></i>
+                            <i class="fa fa-brands fa-react"><FaNodeJs/></i>
                         </div>
                         <div class="ribbon">Node js</div>
                     </div>
@@ -44,42 +46,42 @@ const Skills = () => {
                     <div class="badge red">
                         <div class="circle">
                             {" "}
-                            <i class="fa fa-brands fa-bootstrap"></i>
+                            <i class="fa fa-brands fa-bootstrap"><FaBootstrap/></i>
                         </div>
                         <div class="ribbon">Bootstrap</div>
                     </div>
                     <div class="badge purple">
                         <div class="circle">
                             {" "}
-                            <i class="fa fa-brands fa-java"></i>
+                            <i class="fa fa-brands fa-java"><FaJava/></i>
                         </div>
                         <div class="ribbon">JAVA</div>
                     </div>
                     <div class="badge teal">
                         <div class="circle">
                             {" "}
-                            <i class="fa fa-solid fa-brain"></i>
+                            <i class="fa fa-solid fa-brain"><FaBrain/></i>
                         </div>
                         <div class="ribbon">DSA</div>
                     </div>
                     <div class="badge blue">
                         <div class="circle">
                             {" "}
-                            <i class="fa fa-brands fa-css3-alt"></i>
+                            <i class="fa fa-brands fa-css3-alt"><FaCss3Alt/></i>
                         </div>
                         <div class="ribbon">CSS</div>
                     </div>
                     <div class="badge blue-dark">
                         <div class="circle">
                             {" "}
-                            <i class="fa fa-solid fa-user-secret"></i>
+                            <i class="fa fa-solid fa-user-secret"><TbBrandCpp/></i>
                         </div>
                         <div class="ribbon">C++</div>
                     </div>
                     <div class="badge green">
                         <div class="circle">
                             {" "}
-                            <i class="fa fa-tree"></i>
+                            <i class="fa fa-tree"><FaReact/></i>
                         </div>
                         <div class="ribbon">React</div>
                     </div>
